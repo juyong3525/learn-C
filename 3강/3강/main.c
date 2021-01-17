f340ab95d8dfb82898833e86c11bc2551951311a
Juyong Hong <juyong3525@gmail.com>
2021-01-17 20:53:41 +0900

    added io

---
+++ 3강/3강/main.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	printf("입력받은 숫자는 %d %d %d 입니다.\n", c, b, a);
+	system("pause");
+	return 0;
+}
