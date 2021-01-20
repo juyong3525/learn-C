c099493b45d3c70a61f7e33e6b76c7c5e66a4f8c
Juyong Hong <juyong3525@gmail.com>
2021-01-20 20:32:42 +0900

    Added Pointer

---
+++ C/lecture_c/09-pointer/09-pointer.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/*
+// 포인터 기본
+int main(void)
+{
+    int a = 5;
+    int *b = &a;
+    printf("%d\n", *b);
+    return 0;
+}
+*/
+
+/*
+// 배열 각 원소의 주소값 출력하기
+int main(void)
+{
+    int a[] = {1, 1, 2, 3, 4, 5, 6, 7, 8, 10};
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d\n", &a[i]);
+    }
+    return 0;
+}
+*/
+
+/*
+// 다중 포인터
+int main(void)
+{
+    int a = 5;
+    int *b = &a;
+    int **c = &b;
+    printf("%d\n", **c);
+    return 0;
+}
+*/
+
+// 배열과 포인터
+int main(void)
+{
+    int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int *b = a;
+    printf("%d\n", b[2]);
+}
